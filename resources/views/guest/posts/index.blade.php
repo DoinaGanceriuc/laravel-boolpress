209220da13868be86f21cead036c0db36750fb39
DoinaGanceriuc <doinaganceriuc@gmail.com>
2022-01-26 18:10:16 +0100

    create view guest index

---
+++ resources/views/guest/posts/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row g-3">
+        <h1 class="text-center">News</h1>
+        @foreach ($posts as $post)
+        <div class="col-4">
+            <div class="card h-100">
+                <a href=""><img src="{{ $post->image }}" class="card-img-top" alt="{{ $post->title }}"></a>
+                <div class="card-body">
+                    <h5 class="card-title">{{ strtoupper($post->title) }} </h5>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
+
+@endsection
