5a9eba4421d1783b1e7921c7e504565d9b604a03
DoinaGanceriuc <doinaganceriuc@gmail.com>
2022-01-26 18:09:41 +0100

    create PostController

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::all();
+        return view('guest.posts.index', compact('posts'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        //
+    }
+}
