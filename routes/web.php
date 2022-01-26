9e898d4394e5e7ea86a285f48bfd321c31344d3c
DoinaGanceriuc <doinaganceriuc@gmail.com>
2022-01-26 17:43:14 +0100

    add route resource posts

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,5 @@ Auth::routes();
 
 Route::namespace ('Admin')->prefix('admin')->name('admin.')->middleware('auth')->group(function () {
     Route::get('/', 'HomeController@index')->name('dashboard');
+    Route::resource('posts', 'PostController');
 });
