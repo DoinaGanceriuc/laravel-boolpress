e875229b311d1626a323d303412f14a9308c6d0c
DoinaGanceriuc <doinaganceriuc@gmail.com>
2022-01-27 18:03:32 +0100

    add category in show view

--- resources/views/guest/posts/show.blade.php
+++ resources/views/guest/posts/show.blade.php
@@ -3,12 +3,12 @@
 @section('content')
 <div class="container">
     <div class="d-flex p-5">
-        <img src="{{ $post->image }}" alt="{{ $post->title }}">
+        <img class="img-fluid" src="{{ $post->image }}" alt="{{ $post->title }}">
         <div class="details ms-3">
             <h4 class="title">{{ strtoupper($post->title) }}</h4>
             <p>{{ $post->description }}</p>
-            <div class="author d-flex justify-content-between">
-                <small>Author: {{ $post->author }}</small>
+            <div class="d-flex justify-content-between">
+                <small>Category: {{ $post->category ? $post->category->name : 'Nessuna categoria'}}</small>
                 <small>Posted at: {{ $post->posted_at->format('d/m/Y') }}</small>
             </div>
         </div>
