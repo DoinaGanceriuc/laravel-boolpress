a13830758020faf26ed38a0b855505e304c2c194
DoinaGanceriuc <doinaganceriuc@gmail.com>
2022-01-28 18:25:02 +0100

    create migratio, seeder, controller, model Tag

---
+++ database/seeds/TagSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Models\Tag;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class TagSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tags = ['letteratura', 'pianoforte', 'commedia', 'ricetta', 'arredamento', 'computer'];
+
+        foreach ($tags as $tag) {
+            $new_tag = new Tag();
+            $new_tag->name = $tag;
+            $new_tag->slug = Str::slug($tag);
+            $new_tag->save();
+        }
+
+    }
+}
