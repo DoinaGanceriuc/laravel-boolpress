0c6b54dd808e6d63e6d2807c431459f5cade12f6
DoinaGanceriuc <doinaganceriuc@gmail.com>
2022-01-28 21:17:38 +0100

    fin details index categories

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -44,7 +44,7 @@
                                     <div class="modal-content">
                                         <div class="modal-header">
                                             <h5 class="modal-title" id="{{ $category->id }}Label">Stai cercanando di
-                                                cancellare la categoria {{ $category->title }}</h5>
+                                                cancellare la categoria {{ $category->name }}</h5>
                                             <button type="button" class="btn-close" data-bs-dismiss="modal"
                                                 aria-label="Close"></button>
                                         </div>
