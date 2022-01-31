e5ca7f0aab163c0e01164c799063e33e07ea84ef
DoinaGanceriuc <doinaganceriuc@gmail.com>
2022-01-31 17:45:54 +0100

    fix path in index and show

--- resources/views/guest/posts/index.blade.php
+++ resources/views/guest/posts/index.blade.php
@@ -9,7 +9,8 @@
                     <div class="col-4">
                         <div class="card">
                             <a href="{{ route('guest.posts.show', $post->slug) }}"><img class="w-100"
-                                    src="{{ $post->image }}" class="card-img-top" alt="{{ $post->title }}"></a>
+                                    src="{{ asset('storage/' . $post->image) }}" class="card-img-top"
+                                    alt="{{ $post->title }}"></a>
                             <div class="card-body">
                                 <h5 class="card-title">{{ strtoupper($post->title) }} </h5>
                             </div>
